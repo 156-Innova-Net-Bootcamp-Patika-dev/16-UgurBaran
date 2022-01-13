3a99e3b430ac2846e7c4f2b7b3ffe75a337b606c
UGUR BARAN <ogur.baran@bil.omu.edu.tr>
2022-01-13 13:05:00 +0300

    Updated Comments

--- Week1/BasicSample_Api/BasicSample_Api/Controllers/ToItemsController.cs
+++ Week1/BasicSample_Api/BasicSample_Api/Controllers/ToItemsController.cs
@@ -12,64 +12,64 @@ namespace BasicSample_Api.Controllers
     [Route("api/[controller]")]
     [ApiController]
     public class ToItemsController : ControllerBase
-    {
+                                                                                        {
 
         // test için değişkenler tanımlandı
         public static List<ToItems> items = new List<ToItems>()
-        {
+                                                                                        {
 
-                new ToItems { Id = 1, Name = "Erdi", Surname = "Demir"},
-                new ToItems { Id = 2, Name = "Ozge", Surname = "Acik"},
-                new ToItems { Id = 3, Name = "Kagan", Surname = "Fındık" },
-                new ToItems { Id = 4, Name = "Merve", Surname = "Bilgic" },
-                new ToItems { Id = 5, Name = "Ugur", Surname = "Baran" }
-        };
+                new ToItems { Id = 1, Name = "Erdi", Surname = "Demir"                  },
+                new ToItems { Id = 2, Name = "Ozge", Surname = "Acik"                   },
+                new ToItems { Id = 3, Name = "Kagan", Surname = "Fındık"                },
+                new ToItems { Id = 4, Name = "Merve", Surname = "Bilgic"                },
+                new ToItems { Id = 5, Name = "Ugur", Surname = "Baran"                  }
+                                                                                        };
         
          //GETALL methodu 
          [HttpGet]
         public ActionResult<IEnumerable<ToItems>> GetAllItems()
-        {
+                                                                                        {
            
-            return items;
-        }
+            return items                                                                ;
+                                                                                        }
 
         //Id ye göre filtrelemek için
         [HttpGet("{id}")]
         public ActionResult<ToItems> Get(int id)
         {
             // verilen id ye göre listeyi tarar
-            var item = items.FirstOrDefault(x => x.Id == id);
+            var item = items.FirstOrDefault(x => x.Id == id)                            ;
             if (item == null)
-            {
-                return NotFound("Aranılan kisi bulunamadı");
-            }
+                                                                                        {
+                return NotFound("Aranılan kisi bulunamadı")                             ;
+                                                                                        }
             return item;
-        }
+                                                                                        }
 
         //Ekleme işlemi yapılıyor.
         [HttpPost]
         public IActionResult Post([FromBody] ToItems item)
-        {
-            items.Add(item);
-            return StatusCode(StatusCodes.Status201Created);
-        }
+                                                                                        {
+            items.Add(item)                                                             ;
+            return StatusCode(StatusCodes.Status201Created)                             ;
+                                                                                        }
         
 
 
         // UPDATE  işlemi yapılıyor
         [HttpPut("{id}")]
         public IActionResult Put(int id, [FromBody] ToItems item)
-        {
-            var currentItem = items.Where(x => x.Id == id).FirstOrDefault();
+                                                                                        {
+            var currentItem = items.Where(x => x.Id == id).FirstOrDefault()             ;
 
             if (currentItem != null)
-            {
-                currentItem.Name = item.Name;
-                currentItem.Surname = item.Surname;
+                                                                                        {
+                currentItem.Name = item.Name                                            ;
+                currentItem.Surname = item.Surname                                      ;
 
 
-                return Ok("İslem Basarılı");
-            }
+                return Ok("İslem Basarılı")                                         ;
+                                                                                        }
             else
             {
                 return NotFound("Kisi bulunamadı!");
@@ -84,7 +84,7 @@ namespace BasicSample_Api.Controllers
             if (item != null)
             {
                 items.Remove(item);
-                return NotFound("İşlem Başarılı");
+                return Ok("İslem Basarılı");
             }
             else
             {
